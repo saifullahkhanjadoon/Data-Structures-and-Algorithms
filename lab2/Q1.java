39d7498a4019a2020305e70271ca1e2202252829
saifullahkhanjadoon <skjadoon405@gmail.com>
2025-09-24 12:44:41 +0500

    Update Q1.java

--- lab2/Q1.java
+++ lab2/Q1.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
 public class Q1{
+	  public void insert(int[] arr){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("\nEnter location:   ");
+        int location=sc.nextInt();
+        location-=1;
+        System.out.print("The element to be inserted is: ");
+        int ele=sc.nextInt();
+
+        for(int i=arr.length-1;i>location;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[location]=ele;
+        System.out.print("Output: ");
+        for(int j:arr)
+            System.out.print(j+" ");
+    }
 
 	public static void main(String[] args)
 	{
@@ -10,19 +27,8 @@ public class Q1{
 	}
 	System.out.println();
 
-		Scanner sc=new Scanner(System.in);
+		        Q1 obj = new Q1();
+                obj.insert(numbers);
 
-		System.out.print("\nEnter location:   ");
-		int location=sc.nextInt();
-		location-=1;
-		System.out.print("The element to be inserted is: ");
-		int ele=sc.nextInt();
 
-		for(int i=arr.length-1;i>location;i--){
-			arr[i]=arr[i-1];
-		}
-		arr[location]=ele;
-		System.out.print("Output: ");
-		for(int j:arr)
-			System.out.print(j+" ");
 }}
